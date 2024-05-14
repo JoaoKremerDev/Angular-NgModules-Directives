315a338f1662356028ab8d8e461ec10b87122622
JoaoKremerDev <joaokremer.dev@gmail.com>
2024-05-14 10:14:23 -0300

    feat: ng-content create

---
+++ src/app/exemplo-ngcontent/exemplo-ngcontent.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplo-ngcontent',
+  templateUrl: './exemplo-ngcontent.component.html',
+  styleUrls: ['./exemplo-ngcontent.component.css']
+})
+export class ExemploNgcontentComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
